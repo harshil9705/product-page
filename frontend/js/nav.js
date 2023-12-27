616151cb8dbd3ea1ec95da5353abecea6bf51c0a
harshil lakhan <harshillakhani009.com>
2023-12-27 19:08:22 +0530

    final

---
+++ frontend/js/nav.js
@@ -0,0 +1,35 @@
+let tokenname = document.cookie + "username"
+
+// console.log(token);
+
+let token = document.cookie
+
+let jabar = token.split("=")
+document.getElementById("name").innerHTML = jabar[1] 
+
+if(token){
+    document.getElementById("signup").style.display="none";
+    document.getElementById("login").style.display="none";
+    document.getElementById("name").style.display="block";
+    document.getElementById("logout").style.display="block";
+}
+else{
+    document.getElementById("signup").style.display="block";
+    document.getElementById("login").style.display="block";
+    document.getElementById("name").style.display="none";
+    document.getElementById("logout").style.display="none";
+    
+}
+console.log(token);
+
+function delete_cookie(name) {
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  }
+let logout = jabar[0];
+document.getElementById("logout").addEventListener("click",()=>{
+    //  document.cookies.remove(token)
+    delete_cookie(logout)
+    document.Cookie =  logout + ` path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT `
+    location.reload()
+
+})
